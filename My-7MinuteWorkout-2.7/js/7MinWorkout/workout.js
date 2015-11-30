c9ed0c38b761257b0a225372e60cec66ddefe101
Hoang9x <huyhoang9213@gmail.com>
2015-11-30 14:34:07 +0700

    Completed chapter 2

--- My-7MinuteWorkout-2.7/js/7MinWorkout/workout.js
+++ My-7MinuteWorkout-2.7/js/7MinWorkout/workout.js
@@ -5,7 +5,7 @@
 
     angular
         .module("7minWorkout") // Get existing module
-        .controller("WorkoutController", ['$scope','$interval', WorkoutController]);
+        .controller("WorkoutController", ['$scope', '$interval', '$location', WorkoutController]);
 
     function Exercise(args) {
         this.name = args.name;
@@ -19,21 +19,31 @@
     }
 
     function WorkoutPlan(args) {
-        this.exercises = [];
+        this.exercises = [];//{details:exercie,duration:30},{details:exercie,duration:30},
         this.name = args.name;
         this.title = args.title;
         this.restBetweenExercise = args.restBetweenExercise;
+        this.totalWorkoutDuration = function () {
+            if (this.exercises.length == 0) return 0;
+            var total = 0;
+            angular.forEach(this.exercises, function (exercise) {
+                total = total + exercise.duration;
+            });
+            return this.restBetweenExercise * (this.exercises.length - 1)
+            + total;
+        }
     }
 
 
-    function WorkoutController($scope, $interval) {
+    function WorkoutController($scope, $interval, $location) {
 
+        var vm = this;
         var restExercise;
-        var workoutPlan;
+       // var workoutPlan;
 
-
+        // Start workout
         var startWorkout = function () {
-            workoutPlan = createWorkout();
+            $scope.workoutPlan = createWorkout();
             restExercise = {
                 details: new Exercise({
                     name: "rest",
@@ -41,19 +51,26 @@
                     description: "Relax a bit",
                     image: "img/rest.png"
                 }),
-                duration: workoutPlan.restBetweenExercise
+                duration: $scope.workoutPlan.restBetweenExercise
             };
 
-            startExercise(workoutPlan.exercises.shift());
+            $scope.workoutTimeRemaining = $scope.workoutPlan.totalWorkoutDuration();
 
-            console.log("abc");
+            $interval(function(){
+                $scope.workoutTimeRemaining = $scope.workoutTimeRemaining -1;
+            },1000, $scope.workoutTimeRemaining);
+
+            startExercise($scope.workoutPlan.exercises.shift());
+
+
         };
 
+        // Create workout
         var createWorkout = function () {
             var workout = new WorkoutPlan({
                 name: "7minWorkout",
                 title: "7 minute Workout",
-                restBetweenExercise: 10
+                restBetweenExercise: 2
             });
 
             workout.exercises.push({
@@ -62,11 +79,14 @@
                     title: "Jumping Jacks",
                     description: "Jumping Jacks.",
                     image: "img/JumpingJacks.png",
-                    videos: [],
+
                     variations: [],
-                    procedure: ""
+
+                    videos: ["https://vid.me/SizeMatters", "//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"],
+
+                    procedure: "The first up"
                 }),
-                duration: 30
+                duration: 5
             });
 
             workout.exercises.push({
@@ -75,25 +95,62 @@
                     title: "Wall Sit",
                     description: "A wall sit, also known as a Roman Chair, is an exercise done to strengthen the quadriceps muscles.",
                     image: "img/wallsit.png",
-                    videos: ["//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"],
+                    videos: ["https://vid.me/SizeMatters","//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"],
                     procedure: "Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.\
                               Then, keeping your back against the wall, lower your hips until your knees form right angles. "
                 }),
-                duration: 30
+                duration: 10
             });
 
             return workout;
         };
 
-        var startExercise = function (exercisePlan) {
+        // Start exercise
+        var startExercise = function (exercisePlan) {       
             $scope.currentExercise = exercisePlan;
+            //$scope.nextExerciseTitle = workoutPlan.exercises.length > 0 ? workoutPlan.exercises[0].details.title : "Completed";
             $scope.currentExerciseDuration = 0;
             $interval(function () {
                 ++$scope.currentExerciseDuration;
             }
             , 1000
-            , $scope.currentExercise.duration);
+            , $scope.currentExercise.duration)
+            .then(function () {
+                var nextExercise = getNextExercise($scope.currentExercise);
+                if (nextExercise) {
+                    startExercise(nextExercise);
+                } else {
+                    $location.path('/finish')
+                }
+            });
         };
+
+        // Get next exercise
+        var getNextExercise = function (currentExercise) {
+            var nextExercise = null;
+            if (currentExercise === restExercise) {
+                nextExercise = $scope.workoutPlan.exercises.shift();
+            } else {
+                if ($scope.workoutPlan.exercises.length > 0) {
+                    nextExercise = restExercise;
+                }
+            }
+
+            return nextExercise;
+        };
+
+        // Watch 'currentExerciseDuration'
+        //$scope.$watch('currentExerciseDuration', function (nVal) {
+        //    console.log(nVal);
+        //    if (nVal == $scope.currentExercise.duration) {
+        //        var nextExercise = getNextExercise($scope.currentExercise);
+        //        if (nextExercise) {
+        //            startExercise(nextExercise);
+        //        } else {
+        //            console.log("Workout complete");
+        //        }
+        //    }
+        //});
 
 
         var init = function () {
--- My-7MinuteWorkout-2.7/js/app.js
+++ My-7MinuteWorkout-2.7/js/app.js
@@ -1,12 +1,43 @@
 (function () {
     "use strict";
 
-    angular.module("app", ["7minWorkout"]);
+    // app module
+    angular
+        .module("app", ["ngRoute", "7minWorkout"])
+        .config(function ($routeProvider, $sceDelegateProvider) {
+
+            $sceDelegateProvider.resourceUrlWhitelist([
+                    // Allow same origin resource loads.
+                    'self',
+                    'http://*.youtube.com/**',
+                    'https://vid.me/**']);
+                
+
+            $routeProvider.when('/start', {
+                templateUrl: 'partials/start.html'
+            });
+
+            $routeProvider.when('/workout', {
+                templateUrl: 'partials/workout.html',
+                controller: 'WorkoutController'
+            });
+
+            $routeProvider.when('/finish', {
+                templateUrl: 'partials/finish.html'
+            });
+
+            $routeProvider.otherwise({
+                redirectTo : '/start'
+            });
+
+});
 
 
-    // Creating a new module name is 7minWorkout
-    angular.module("7minWorkout", []);
+// Creating a new module name is 7minWorkout
+    angular
+        .module("7minWorkout",
+            []);
 
-    asdadaasd
+    
     
 })();
